54d7331032d42943fbe789d6542961a49a5e8ca5
John Wiegley <johnw@newartisans.com>
2014-09-16 20:53:31 +0100

    Can now define and reference variables

--- Parser.hs
+++ Parser.hs
@@ -4,19 +4,26 @@ module Parser where
 
 import Control.Applicative
 import Control.Monad
+import Control.Monad.IO.Class
+import Control.Monad.Trans.State
+import Data.Functor.Identity
+import Data.Map
 import Data.Text as T
+import Data.Text.IO as T
 import Debug.Trace
 import Text.Parsec.Char
 import Text.Parsec.Combinator hiding (optional)
-import Text.Parsec.Prim hiding (many, (<|>))
+import Text.Parsec.Prim hiding (many, (<|>), State)
 import Text.Parsec.Text
-import Text.Parsec.Token hiding (whiteSpace, symbol)
+import Control.Monad.Morph (hoist)
 
 data Expr
     = EAdd Expr Expr
     | ESub Expr Expr
     | EMul Expr Expr
     | EDiv Expr Expr
+    | ELet Text Expr
+    | EVar Text
     | ENum Double
     deriving Show
 
@@ -36,16 +43,27 @@ num = trace "num" $ ENum . read <$> go (oneOf "0123456789")
             Just _ -> (\y -> x ++ "." ++ y) <$> many np
             Nothing -> return x
 
+varname :: Parser Text
+varname = pack <$> some letter
+
+variable :: Parser Expr
+variable = EVar <$> varname
+
 term :: Parser Expr
-term = trace "term" $ choice [ num ] <* whiteSpace
+term = trace "term" $ choice terms <* whiteSpace
+  where
+    terms = [ letBinding
+            , variable
+            , try num
+            ]
 
 addop :: Parser (Expr -> Expr -> Expr)
-addop = trace "addop" $ EAdd <$ (trace "1.." (symbol "+"))
-    <|> ESub <$ (trace "2.." (symbol "-"))
+addop = trace "addop" $ EAdd <$ symbol "+"
+    <|> ESub <$ symbol "-"
 
 mulop :: Parser (Expr -> Expr -> Expr)
-mulop = trace "mulop" $ EMul <$ (symbol "*")
-    <|> EDiv <$ (string "/" <* whiteSpace)
+mulop = trace "mulop" $ EMul <$ symbol "*"
+    <|> EDiv <$ string "/"
 
 expr :: Parser Expr
 expr = trace "expr" $ subexpr `chainl1` addop
@@ -59,12 +77,43 @@ parenthesis = between (symbol "(") (symbol ")")
 factor :: Parser Expr
 factor = parenthesis expr <|> term
 
-evalExpr :: Expr -> Double
-evalExpr (ENum n) = n
-evalExpr (EAdd x y) = evalExpr x + evalExpr y
-evalExpr (ESub x y) = evalExpr x - evalExpr y
-evalExpr (EMul x y) = evalExpr x * evalExpr y
-evalExpr (EDiv x y) = evalExpr x / evalExpr y
+letBinding :: Parser Expr
+letBinding = trace "letBinding" $ do
+    trace "letBinding Parser.hs:81.." $ return ()
+    symbol "let"
+    trace "letBinding Parser.hs:83.." $ return ()
+    name <- varname
+    trace "letBinding Parser.hs:85.." $ return ()
+    symbol "="
+    trace "letBinding Parser.hs:87.." $ return ()
+    body <- expr
+    return $ ELet name body
+
+newtype Env = Env { getEnv :: Map Text Double }
+
+newEnv :: Env
+newEnv = Env Data.Map.empty
+
+eval :: Expr -> Double
+eval x = runIdentity $ evalStateT (evalExpr x) newEnv
+
+evalExpr :: Expr -> State Env Double
+evalExpr (ENum n)   = return n
+evalExpr (EAdd x y) = (+) <$> evalExpr x <*> evalExpr y
+evalExpr (ESub x y) = (-) <$> evalExpr x <*> evalExpr y
+evalExpr (EMul x y) = (*) <$> evalExpr x <*> evalExpr y
+evalExpr (EDiv x y) = (/) <$> evalExpr x <*> evalExpr y
+
+evalExpr (ELet n b) = do
+    b' <- evalExpr b
+    modify $ Env . insert n b' . getEnv
+    return b'
+
+evalExpr (EVar n) = do
+    env <- get
+    case Data.Map.lookup n (getEnv env) of
+        Nothing -> error $ "Unknown variable: " ++ unpack n
+        Just b  -> return b
 
 massage :: Text -> Maybe Text
 massage txt =
@@ -78,18 +127,26 @@ evalText str =
         Nothing -> ""
         Just input -> pack $ case parse expr "" input of
             Left e  -> show e
-            Right x -> show (evalExpr x)
+            Right x -> show (eval x)
 
 test :: Text -> IO ()
-test = putStrLn . unpack . evalText
+test = T.putStrLn . evalText
 
 testFile :: FilePath -> IO ()
 testFile path = do
-    str <- readFile path
-    forM_ (T.lines (pack str)) test
+    str <- T.readFile path
+    forM_ (T.lines str) test
     -- case parse (many expr <* eof) path (pack str) of
     --     Left e   -> error (show e)
     --     Right xs -> forM_ xs $ print . evalExpr
 
 repl :: IO ()
-repl = forever $ test . pack =<< getLine
+repl = do
+    _ <- flip execStateT newEnv $ forever $ do
+        input <- liftIO T.getLine
+        case parse expr "" input of
+            Left e  -> liftIO $ print e
+            Right x -> do
+                a <- hoist (return . runIdentity) (evalExpr x)
+                liftIO $ print a
+    return ()
